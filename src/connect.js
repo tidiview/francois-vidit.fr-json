ff0415e3ded33a53428637696545ddba971471cc
tidiview <francois.vidit@laposte.net>
2021-01-18 21:22:50 +0100

    got rid of sensible connexion data to env

--- src/connect.js
+++ src/connect.js
@@ -1,10 +1,14 @@
 const { MongoClient } = require("mongodb");
+require('dotenv').config();
 
- 
+
 // Replace the following with your Atlas connection string
 
-const url = "mongodb+srv://tidiview:RTNY3U6npVZj6ft@cluster0.l5krc.mongodb.net/test?retryWrites=true&w=majority";
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
 
+const url = "mongodb+srv://" + user + ":" + pass + "@" + host + "/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
 const client = new MongoClient(url);
 
 async function run() {
