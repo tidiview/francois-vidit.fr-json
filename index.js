7ef19dd27c555069ef8bf23386c6e17e9cc25b30
tidiview <francois.vidit@laposte.net>
2021-01-23 19:59:49 +0100

    actualisation 20210123_1959

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-const mongoose = require("mongoose");
-require('dotenv').config();
+const polka = require('polka');
+const { json } = require('body-parser');
+const { PORT=4000 } = process.env;
+
+polka()
+  .use(json())
+  .get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json', 'x-content-type-options': 'nosniff', 'cache-control': 'no-cache', 'content-type': 'utf-8' });
+    let json = JSON.stringify(req.body);
+    res.end(json);
+  })
+  .listen(PORT, err => {
+    if (err) throw err;
+    console.log(`> Running on localhost:${PORT}`);
+});
+
+const { MongoClient } = require("mongodb");
+require('dotenv').config()
+
+// Replace the following with your Atlas connection string
 
 const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const pass = process.env.DB_PASS;
 
-const uri = "mongodb+srv://" + user + ":" + pass + "@" + host + "/test?retryWrites=true&w=majority";
+const url = "mongodb+srv://" + user + ":" + pass + "@" + host + "/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+const client = new MongoClient(url);
 
-mongoose.connect(uri, {
-  useNewUrlParser:true, 
-  useUnifiedTopology:true, 
-  useCreateIndex: true,
-	useFindAndModify: false
-})
+// The database to use
+const dbName = "test";
 
-mongoose.connection.on("error", function(error) {
-  console.log(error)
-})
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db(dbName);
 
-mongoose.connection.on("open", function() {
-  console.log("Connected to MongoDB database.")
-})
+    // Use the collection "people"
+    const col = db.collection("people");
+
+    // Find one document
+    const myDoc = await col.find().toArray();
+    // Print to the console
+    console.log(myDoc);
+
+    } catch (err) {
+      console.log(err.stack);
+  }
+
+  finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir); 
