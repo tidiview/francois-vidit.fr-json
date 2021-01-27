62ed4c4499d53e9f938d9125f06b0d850b72adfa
tidiview <francois.vidit@laposte.net>
2021-01-27 16:53:28 +0100

    Updated index.js on the way to CRUD requests: added Delete POST Method

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const polka = require('polka');
-const send = require('@polka/send-type');
+const { json } = require('body-parser');
 const { PORT=3000 } = process.env;
+const send = require('@polka/send-type');
 const posts = require('./posts');
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId, ObjectID } = require("mongodb");
 require('dotenv').config()
-/* const { json } = require('body-parser'); */
 /* const fetch = require('node-fetch'); */
 
 // Mongodb credentials
@@ -13,6 +13,48 @@ const user = process.env.DB_USER;
 const pass = process.env.DB_PASS;
 
 const url = "mongodb+srv://" + user + ":" + pass + "@" + host + "/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+
+const payloadMitsue = {
+  "name": { "first": "光江", "last": "Vidit" }
+}
+ 
+// The database to use
+/* const dbName = "test";
+
+// The collection to use
+const collectionName = "people";
+
+const client = new MongoClient(url);
+
+const ObjectIDMitsue = "6011866c24ede92c7022867d";
+
+const init = async () => {
+  
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db(dbName);
+
+    // Use the collection "people"
+    const col = db.collection(collectionName);
+
+    // Insert a document
+    // const myDoc = await col.insertOne(payloadMitsue);
+
+    // Delete a document
+    const myDoc = await col.deleteOne({_id: ObjectID(ObjectIDMitsue)});
+
+    // Print to the console
+    console.log(myDoc);
+
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+}
+
+init(); */
 
 polka()
   .use(json())
@@ -27,8 +69,54 @@ polka()
       // res: ServerReponse | code: Number | data: String | headers: Object
       send(res, 200, data, { 'Content-Type': 'application/json; charset=UTF-8', 'X-Content-Type-Options': 'nosniff', 'Cache-Control': 'no-cache' });
   })
+
+  /* .post('/', (req, res) => {
+		res.writeHead(200, { 'Content-Type': 'application/json; charset=UTF-8', 'X-Content-Type-Options': 'nosniff', 'Cache-Control': 'no-cache' });
+		let json = JSON.stringify(req.body);
+		res.end(json);
+  }) */
+  
+  .post('/', (req, res) => {
+    let json = JSON.stringify(req.body);
+    
+    // The database to use
+    const dbName = "test";
+
+    // The collection to use
+    const collectionName = "people";
+
+    const client = new MongoClient(url);
+
+    const init = async () => {
+  
+      try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+    
+        // Use the collection "people"
+        const col = db.collection(collectionName);
+    
+        // Find documents
+        const myDoc = await col.insertOne(JSON.parse(json));
+        // Print to the console
+        console.log(myDoc);
+    
+      } catch (err) {
+        console.log(err.stack);
+      } finally {
+        await client.close();
+      }
+    }
+    
+    init();
+
+		res.writeHead(200, { 'Content-Type': 'application/json; charset=UTF-8', 'X-Content-Type-Options': 'nosniff', 'Cache-Control': 'no-cache' });
+    res.end(json)
+  })
+    
   .use("posts", posts)
   .listen(PORT, err => {
     if (err) throw err;
-    console.log(`> Running on localhost:${PORT}`);
-});
+    console.log( `> Running on localhost:${PORT}` );
+  });
